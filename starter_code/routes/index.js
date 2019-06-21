95d63ea941779ed3a303f02385a3d5bd8c728646
Grégoire Lescuyer <gregoirelescuyer@gmail.com>
2019-06-21 11:48:16 +0200

    done without CSS

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const movieModel = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  movieModel
+    .find({})
+    .then(dbRes => {
+      res.render("movies", { movies: dbRes });
+    })
+    .catch(dbErr => {
+      res.render("movies", dbErr);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  movieModel
+    .findById(req.params.id)
+    .then(movie => {
+      res.render("movieDetails", { movie });
+    })
+    .catch(err => {
+      console.log("there is a error in DB", err);
+    });
 });
 
 module.exports = router;
